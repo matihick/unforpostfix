9aaea1254da0ecc9298989ce2b8225ecefaae296
unformatt <unformatt@gmail.com>
2013-09-10 13:09:50 -0300

    fixed email list validator

--- app/validators/email_list_validator.rb
+++ app/validators/email_list_validator.rb
@@ -1,6 +1,6 @@
 class EmailListValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.split(',').all? { |v| v =~ /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i }
+    unless value.split(',').all? { |v| v.strip =~ /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i }
       record.errors[attribute] << (options[:message] || I18n.t('activerecord.errors.messages.email_list'))
     end
   end
