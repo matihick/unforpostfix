906a54d5c526fbfe0682a657f4f71624912a7122
unformatt <unformatt@gmail.com>
2013-09-10 07:44:51 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postfixman::Application.config.secret_key_base = '3da6b1979985d43e5663d4717d6832f0de2cc6addd586f110a56a960c7d7339ef7508e33c766c04dd6da5655d236081eb3a4b8a4ba3c24305239d7417a3a5d80'
