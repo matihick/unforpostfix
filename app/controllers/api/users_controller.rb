8a4deae19cc5de566283471f4f68a33e63027966
unformatt <unformatt@gmail.com>
2017-03-06 12:28:12 -0300

    fix updating user password

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,7 +8,8 @@ class Api::UsersController < Api::BaseApiController
       domain_id: Domain.enabled.find(params[:domain_id]).id,
       name: params[:name],
       email: params[:email],
-      unencrypted_password: params[:password]
+      unencrypted_password: params[:password],
+      password_confirmation: params[:password]
     }.keep_if { |k,v| v.present? })
 
     @user.save!
@@ -21,7 +22,8 @@ class Api::UsersController < Api::BaseApiController
       name: params[:name],
       email: params[:email],
       enabled: params[:enabled],
-      unencrypted_password: params[:password]
+      unencrypted_password: params[:password],
+      password_confirmation: params[:password]
     }.keep_if { |k,v| v.present? }
 
     attrs.delete_if { |k,v| v.nil? }
